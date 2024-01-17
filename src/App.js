a5820116d2fe70e463e6f0103ba2ea51922f0d35
Ercan <ercann.6191@gmail.com>
2024-01-17 23:01:49 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+
+  const [nasaVeri, setNasaVeri] = useState(null);
+  const [day, setDay] = useState(1);
+
+  useEffect(() => {
+    axios.get("https://api.nasa.gov/planetary/earth/assets?lon=100.75&lat=1.5&date=2014-02-01&dim=0.15&api_key=DEMO_KEY&date=2012-11-"+day);
+    .then(res => setNasaVeri(res.data))
+    .catch(err => console.log(err))
+  }, [])
+
   return (
     <div className="App">
       <p>
@@ -13,3 +24,9 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+
