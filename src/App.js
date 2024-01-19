31ffab9de13203ed79f3a58751e6970d29f83904
Ercan <ercann.6191@gmail.com>
2024-01-19 22:00:26 +0300

    another commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
+import Nasa from "./mainComponents/Nasa";
+import { dummyData } from "./mainComponents/dummyData";
 
 function App() {
 
-  const [nasaVeri, setNasaVeri] = useState(null);
+  const [data, setData] = useState(dummyData);
   const [day, setDay] = useState(1);
 
+  const newDay = (days) =>{
+    let gun = new Date (day);
+    gun.setDate(gun.getDate() + days);
+    let gercekTarih = `${gun.getFullYear()+1}-${gun.getMonth()+1}-${gun.getDate()}`
+    setDay(gercekTarih);
+   
+  }
+
   useEffect(() => {
-    axios.get("https://api.nasa.gov/planetary/earth/assets?lon=100.75&lat=1.5&date=2014-02-01&dim=0.15&api_key=DEMO_KEY&date=2012-11-"+day);
-    .then(res => setNasaVeri(res.data))
+    axios.get("https://api.nasa.gov/planetary/earth/assets?lon=100.75&lat=1.5&date=2014-02-01&dim=0.15&api_key=DEMO_KEY&date=2012-11-"+day)
+    .then(res => setData(res.data))//gelen veriler hook ile değişken olan date in içine aktarılacak
     .catch(err => console.log(err))
-  }, [])
+  }, [day]); //gün değiştiğinde gelen bilgiler de değişecek
 
   return (
-    <div className="App">
-      <p>
-        NASA uygulamasını yapmak için README.md dosyasıdaki talimatları takip edin
-		İyi eğlenceler! <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+    <div className="App"> 
+      <button onClick = {newDay(-1)}>Azalt</button>
+      <button onClick = {newDay(+1)}>Artır</button>
+
+      <Nasa veri = {data}/>
+
+     
     </div>
   );
 }
 
 export default App;
-
 
 
 
---
+++ src/mainComponents/Nasa.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Explanation from "./Explanation";
+import Image from "./Image";
+import Copyright from "./Copyright";
+import Date from "./Date";
+import Title from
+
+
+
+function Nasa(props) {
+
+    const {title, date, copyright,url, media_type, explanation} = props.data
+    return(
+        <div className="nasa">
+           <Title title ={title} />
+           <Date date = {date} />
+           <Copyright copyright = {copyright} />
+           <Image url = {url} media = {media_type} />
+
+           <Explanation explanation ={explanation} />
+        </div>
+    )
+}
+
+export default Nasa;
